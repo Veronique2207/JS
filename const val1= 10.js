13f8e2e347f15ad669b44085f2c336cc4347eae8
Veronique2207 <bulongoveronique@gmail.com>
2022-08-10 14:25:12 +0200

    js challenge

---
+++ const val1= 10.js
@@ -0,0 +1,14 @@
+const val1= 10
+const val2 = 5
+
+const result = val1*val2 
+
+console.log (val1*val2)
+
+const firstName = 'Amamda'
+const lastName =  'Jacobs'
+const fullName = $ {'Amanda'} + ${'Jacobs'}
+console.log(fullName)
+
+//output: Amanda Jacobs
+
