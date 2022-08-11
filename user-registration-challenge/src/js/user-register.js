d36a83ecd6ebd232bb78d2324e0231aa85725535
Veronique2207 <bulongoveronique@gmail.com>
2022-08-11 21:01:21 +0200

    todo app

---
+++ user-registration-challenge/src/js/user-register.js
@@ -0,0 +1,9 @@
+
+    const registerTab = document.getElementsByClassName("register");
+    console.log(registerTab);
+
+    
+    const userTabs = document.getElementsByClassName("users");
+    console.log(userTabs);
+
+
